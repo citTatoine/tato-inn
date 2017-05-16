1ee24cf10f63b69b8cab86881e8a2da3bcbe6890
kennymango <kennethsitu@gmail.com>
2017-05-16 11:05:21 -0700

    small fix in webpack for angular implementation
    Signed-off-by: kennymango <kennethsitu@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,10 @@ var bF = path.resolve(__dirname, "build");
 
 var config = {
     entry: {
-        //"login":jF+"/login.js",
+        "login":jF+"/login.js",
         //vendor: ['angular'],
-        //"admin":jF+"/admin.js",
-        "order":jF+"/order.js",
+        "admin":jF+"/admin.js",
+        "order":jF+"/orderpage.js",
         "owl":jF+"/owl.carousel.min.js",
         "kitchen":jF+"/kitchen.js"
     },
