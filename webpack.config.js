8655d2b9922f3e46b28af5394afeef330b5d74b1
kennymango <kennethsitu@gmail.com>
2017-05-16 13:32:38 -0700

    resolve merge conflict
    Signed-off-by: kennymango <kennethsitu@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,9 @@ var config = {
         "login":jF+"/login.js",
         //vendor: ['angular'],
         "admin-angular":jF+"/admin-angular.js",
-<<<<<<< HEAD
         "order":jF+"/order.js",
-=======
         "admin":jF+"/admin.js",
         "order":jF+"/orderpage.js",
->>>>>>> 8ff476cb03b0421efd7f11ae0d17cce989f87aff
         "owl":jF+"/owl.carousel.min.js",
         "kitchen":jF+"/kitchen.js"
     },
